fd64bf6e2ea4336a7eedb995f5ceda99e985ceed
ido issaki <ido2001@gmail.com>
2026-03-08 18:58:56 +0200

    added parcer.h

---
+++ parcer.h
@@ -0,0 +1,16 @@
+/*
+ * קובץ: parser.h
+ */
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "globals.h"
+
+boolean is_reserved_word(const char *word);
+boolean is_valid_label_name(const char *name);
+void extract_word(char **source, char *dest);
+boolean is_label_definition(const char *word);
+boolean is_valid_number(const char *str);
+boolean is_valid_string_literal(const char *str);
+
+#endif
