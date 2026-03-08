105d9def9bd8b9a06d7ccda3d15f6a70cc999b44
ido issaki <ido2001@gmail.com>
2026-03-08 19:32:06 +0200

    added main.c

---
+++ main.c
@@ -0,0 +1,52 @@
+/*
+ * קובץ: main.c (מיועד לבדיקת שלב הקדם-אסמבלר בלבד)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "globals.h"
+#include "utils.h"
+#include "pre_assembler.h"
+
+int main(int argc, char *argv[]) {
+    int i;
+    FILE *source_file;
+    char *file_name;
+    AssemblerContext current_context;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file1> <file2> ...\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (i = 1; i < argc; i++) {
+        file_name = create_file_name(argv[i], ".as");
+        source_file = fopen(file_name, "r");
+        
+        if (source_file == NULL) {
+            fprintf(stderr, "Error: Cannot open file '%s'. Skipping to next.\n", file_name);
+            free(file_name);
+            continue;
+        }
+
+        /* אתחול ההקשר ללא משתנים גלובליים */
+        current_context.ic = INITIAL_IC; 
+        current_context.dc = 0;
+        current_context.line_number = 1;
+        current_context.error_found = FALSE;
+        current_context.symbol_head = NULL;
+
+        printf("Processing macros for file: %s\n", file_name);
+        
+        /* הפעלת שלב הפרישה בלבד */
+        if (pre_assemble(source_file, argv[i], &current_context)) {
+            printf("Success! Generated %s.am\n", argv[i]);
+        } else {
+            printf("Failed: Errors found in %s\n", file_name);
+        }
+
+        fclose(source_file);
+        free(file_name);
+    }
+
+    return EXIT_SUCCESS;
+}
