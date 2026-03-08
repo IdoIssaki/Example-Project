f90fa41bd4030e08dfbfd056491c9855f58f5119
ido issaki <ido2001@gmail.com>
2026-03-08 20:04:45 +0200

    changed utils.c

--- utils.c
+++ utils.c
@@ -1,15 +1,42 @@
 /*
- * קובץ: utils.h
+ * קובץ: utils.c
  */
-#ifndef UTILS_H
-#define UTILS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
 
-#include <stddef.h>
-#include "globals.h"
+void *safe_malloc(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        fprintf(stderr, "Fatal Error: Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
 
-void *safe_malloc(size_t size);
-void skip_whitespaces(char **str);
-boolean is_empty_or_comment(char *line);
-char *create_file_name(const char *original_name, const char *extension);
+void skip_whitespaces(char **str) {
+    if (str == NULL || *str == NULL) return;
+    while (**str == ' ' || **str == '\t') {
+        (*str)++;
+    }
+}
 
-#endif
+boolean is_empty_or_comment(char *line) {
+    char *ptr = line;
+    if (line == NULL) return TRUE;
+    skip_whitespaces(&ptr);
+    if (*ptr == '\0' || *ptr == '\n' || *ptr == ';') return TRUE;
+    return FALSE;
+}
+
+char *create_file_name(const char *original_name, const char *extension) {
+    char *full_name;
+    size_t length;
+    if (original_name == NULL || extension == NULL) return NULL;
+    length = strlen(original_name) + strlen(extension) + 1;
+    full_name = (char *)safe_malloc(length);
+    strcpy(full_name, original_name);
+    strcat(full_name, extension);
+    return full_name;
+}
